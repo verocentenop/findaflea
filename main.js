707fa797290ceca6813354d01d44d179be3fd9ea
verocentenop <159185910+verocentenop@users.noreply.github.com>
2024-08-13 13:58:26 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import './style.css'
 import { printHeader } from '/src/Header/Header'
 import { printFunction } from '/src/Function/Function'
+import { printRegisterForm } from './src/Form/Form'
 
 const init = async () => {
   printHeader()
   printFunction()
+  printRegisterForm()
 }
 init()
